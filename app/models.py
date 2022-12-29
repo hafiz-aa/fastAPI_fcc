ab01108e42c0acf2301595a391549102472b8cc5
hafiz-aa <mhafiz.aa@gmail.com>
2022-12-29 14:27:10 +0700

    Edit table models

--- app/models.py
+++ app/models.py
@@ -9,4 +9,4 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, nullable=False, default=True)
+    published = Column(Boolean,  default=True)
