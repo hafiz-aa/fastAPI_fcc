a30d75eb96da10b3a3e32f09370c1ff2f6c24f90
hafiz-aa <mhafiz.aa@gmail.com>
2023-01-02 16:00:44 +0700

    Add limit & skip

--- app/routers/post.py
+++ app/routers/post.py
@@ -11,10 +11,11 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+    print(skip)
+    posts = db.query(models.Post).limit(limit).offset(skip).all()
     return posts
 
 # Post a post
