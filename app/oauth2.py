0fc048a54deb9aede43dbeeb95ca8628a8ceee99
hafiz-aa <mhafiz.aa@gmail.com>
2023-01-02 09:10:10 +0700

    Edit typo

--- app/oauth2.py
+++ app/oauth2.py
@@ -17,7 +17,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
 	to_encode = data.copy()
 
-	expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+	expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 	to_encode.update({"exp": expire})
 
 	encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -28,7 +28,7 @@ def verify_access_token(token: str, credentials_exception):
 
 	try: 
 		
-		payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+		payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 		id: str = payload.get("user_id")
 
 		if id is None:
@@ -38,6 +38,8 @@ def verify_access_token(token: str, credentials_exception):
 
 	except JWTError:
 		raise credentials_exception
+
+	return token_data
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
 	credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
