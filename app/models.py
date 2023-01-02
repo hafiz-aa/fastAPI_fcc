2767d5d9f574e57585c29271a2efc42b9c2a0f22
hafiz-aa <mhafiz.aa@gmail.com>
2023-01-02 11:35:57 +0700

    Add owner_id to post

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
@@ -13,7 +13,7 @@ class Post(Base):
     published = Column(Boolean,  server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
-
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable = False)
 
 class User(Base):
     __tablename__ = "users"
