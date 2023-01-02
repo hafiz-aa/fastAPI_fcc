4fb011f2f6d00f21c8401ca5dd3a3f82f1a44ac0
hafiz-aa <mhafiz.aa@gmail.com>
2023-01-02 13:47:24 +0700

    Add oauth2 to delete post

--- app/routers/post.py
+++ app/routers/post.py
@@ -60,13 +60,19 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int =  Dep
     # deleted_post = cursor.fetchone()
 
     # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    
+    post = post_query.first()
 
-    if post.first() == None:
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
 
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+      raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail= "Not authorized to perform requested action")	
+
+
+    post_query.delete(synchronize_session=False)
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
