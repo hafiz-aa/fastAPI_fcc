d39531186f76dbc0ea43aea0eea19a4434f0372c
hafiz-aa <mhafiz.aa@gmail.com>
2023-01-04 09:57:53 +0700

    Setting alembic

---
+++ alembic/versions/e66bc55565bd_create_posts_table.py
@@ -0,0 +1,24 @@
+"""create posts table
+
+Revision ID: e66bc55565bd
+Revises: 
+Create Date: 2023-01-04 09:55:43.105628
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e66bc55565bd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
